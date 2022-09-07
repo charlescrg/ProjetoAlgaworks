348ccf6cfc7f6b54ee39441319e7ff199114edc2
charlescrg <charles_crg@hotmail.com>
2022-09-07 20:54:07 -0300

    alterar pessoa pt1

--- src/main/java/com/example/algamoney/api/repository/PessoaRepository.java
+++ src/main/java/com/example/algamoney/api/repository/PessoaRepository.java
@@ -1,8 +1,17 @@
 package com.example.algamoney.api.repository;
 
+
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.algamoney.api.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+	public void save(Optional<Pessoa> pessoaSalva);
+
+
 	
 }
