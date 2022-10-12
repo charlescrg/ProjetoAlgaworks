86836e18cbc312a888ad1b0d6c90375ecccb6096
charlescrg <charles_crg@hotmail.com>
2022-10-12 13:28:08 -0300

    filtro de lancamentos

--- src/main/java/com/example/algamoney/api/repository/LancamentoRepository.java
+++ src/main/java/com/example/algamoney/api/repository/LancamentoRepository.java
@@ -3,8 +3,9 @@ package com.example.algamoney.api.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.repository.lancamento.LancamentoRepositoryQuery;
 
-public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
 
 	public void save(Optional<Lancamento> lancamentoSalva);
 
--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -32,6 +32,7 @@ import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.exceptionhandler.AlgamoneyExceptionHandler.Erro;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.filter.LancamentoFilter;
 import com.example.algamoney.api.service.LancamentoService;
 //import com.example.algamoney.api.service.LancamentoService;
 import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
@@ -53,8 +54,8 @@ public class LancamentoResource {
 	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
-	public List<Lancamento> listar(){
-		return lancamentoRepository.findAll();
+	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter){
+		return lancamentoRepository.filtrar(lancamentoFilter);
 	}
 	
 	@GetMapping("/{codigo}")
